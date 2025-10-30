e619dad2c81766002adf01a639cc3de5cd5c2adc
sivitzhaky <itzhaky.sivan@gmail.com>
2025-10-30 09:35:03 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -9,6 +9,7 @@ def find_max_number(num1, num2, num3):
 def find_mean(num1, num2, num3):
     return ((num1 + num2 + num3)/3)
     
+import math
 def find_mean_std(num1, num2, num3):
     mean1 = find_mean(num1, num2, num3)
     std = math.sqrt(((num1-mean1)**2+(num2-mean1)**2+(num3-mean1)**2)/3)
